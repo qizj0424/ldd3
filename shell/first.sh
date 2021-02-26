64630339219654326fb03f900e384765e4ac02e3
zjqi <zhengjian.qi@ingenic.com>
2021-02-26 14:43:10 +0800

    add shell dir,add shell/first.sh

---
+++ shell/first.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Hello World !"
+
+your_name="runoob"
+
+# 使用双引号拼接
+greeting="hello, "$your_name" !"
+greeting_1="hello, ${your_name} !"
+echo $greeting  $greeting_1
+
+# 使用单引号拼接
+greeting_2='hello, '$your_name' !'
+greeting_3='hello, ${your_name} !'
+echo $greeting_2  $greeting_3
